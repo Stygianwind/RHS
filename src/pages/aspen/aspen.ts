a040cfa8f687dd6d4a65c381adc3ba0a67307142
Stygianwind <ikarthikiyer@gmail.com>
2017-12-30 10:32:39 -0500

    initial aspen browser

--- src/pages/aspen/aspen.ts
+++ src/pages/aspen/aspen.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 /**
  * Generated class for the AspenPage page.
@@ -15,10 +16,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AspenPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private iab: InAppBrowser) {
   }
-
+  
   ionViewDidLoad() {
+    const aspenBrowser = this.iab.create('https://nj-bernards.myfollett.com/aspen/logon.do');
     console.log('ionViewDidLoad AspenPage');
   }
 
